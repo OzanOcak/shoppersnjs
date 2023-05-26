f81305db42b7809b01304b139433d733a3d917a8
OzanOcak <ozanocak@gmail.com>
2023-05-26 21:34:00 +0300

    productimg

--- components/Product.tsx
+++ components/Product.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import ProductImage from './ProductImage'
 
 type Props = { product: Product }
 
@@ -9,6 +10,10 @@ function Product({ product }: Props) {
       href={`/product/${product.id}`}
       className='h-96 flex flex-col p-5 rounded border group hover:scale-105 transition-transform ease-out duration-200'
     >
+      <div className='relative max-h-72 flex-1'>
+        <ProductImage product={product} fill />
+      </div>
+
       <div className='font-semibold flex items-center justify-between mt-4 mb-1'>
         <p className='w-44 truncate'>{product.title}</p>
         <p>{product.price}</p>
